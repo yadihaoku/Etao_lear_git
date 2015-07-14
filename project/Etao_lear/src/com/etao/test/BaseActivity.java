5fc0360da1c691e0bad66707e7776876eac583c2
yanyadi <yadihaoku@126.com>
2015-07-15 00:01:27 +0800

    gitignore文件添加结束的空行

---
+++ project/Etao_lear/src/com/etao/test/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.etao.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+
+}
